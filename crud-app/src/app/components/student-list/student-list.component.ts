284b6f7d5399b2ef64452c2ef3971512663adf17
MadHead34 <mingh2865@gmail.com>
2025-03-15 22:42:07 +1300

    Updated Features for Frontend and improving api

--- crud-app/src/app/components/student-list/student-list.component.ts
+++ crud-app/src/app/components/student-list/student-list.component.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { StudentService } from '../../services/student.service';
 import { Student } from '../../models/student.model';
 
 @Component({
   selector: 'app-student-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule], 
   templateUrl: './student-list.component.html',
-  // styleUrls: ['./student-list.component.css']
 })
 export class StudentListComponent implements OnInit {
   students: Student[] = [];
+  filteredStudents: Student[] = [];
+  totalStudents = 0;
+  selectedFilter = 'all';
 
   constructor(private studentService: StudentService) {}
 
   ngOnInit(): void {
+    this.fetchStudents();
+  }
+
+  fetchStudents(): void {
     this.studentService.getStudents().subscribe((data) => {
       this.students = data;
+      this.filteredStudents = data;
+      this.totalStudents = data.length;
     });
+  }
+
+  filterStudents(filter: string): void {
+    this.selectedFilter = filter;
+    if (filter === 'all') {
+      this.filteredStudents = this.students;
+    } else if (filter === 'no-subjects') {
+      this.studentService.getStudentsWithoutSubjects().subscribe((data) => {
+        this.filteredStudents = data;
+      });
+    }
   }
 }
--- crud-app/src/app/models/student.model.ts
+++ crud-app/src/app/models/student.model.ts
@@ -2,5 +2,7 @@ export interface Student {
     id: number;
     name: string;
     age: number;
+    courseName: string;
+    subjectNames: string;
     courseId: number;
   }
